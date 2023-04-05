04766aac3df9fee5307c28fe64a7f6e923f3feaf
ansonshendrick <113936107+ansonshendrick@users.noreply.github.com>
2023-04-04 20:15:17 -0500

    Javascript zipcode api fetch
    Javascript zipcode api fetch

--- js/init.js
+++ js/init.js
@@ -6,3 +6,26 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+async function findNearestBars() {
+  const zipcodeInput = document.getElementById("zipcode-input");
+  const by_postal = zipcodeInput.value;
+  console.log(by_postal)
+  const response = await fetch(`https://api.openbrewerydb.org/v1/breweries?by_postal=${by_postal}&per_page=3`);
+  const data = await response.json();
+
+  if (data.length === 0) {
+    console.log("No bars found in this area.");
+    return;
+  }
+
+  const nearestBars = data.slice(0, 5);
+  nearestBars.forEach(bar => {
+    console.log(data);
+  });
+}
+
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  findNearestBars();
+});
